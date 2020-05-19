09c30e0da7ae70856f06ac6f1e9a7a0aee2d7f67
Kfir Yehezkel <kfir.ye@gmail.com>
2020-05-19 17:15:41 +0300

    updates sources to be empty.

--- src/main/java/com/DateCalculator.java
+++ src/main/java/com/DateCalculator.java
@@ -1,12 +1,5 @@
 package com;
 
 public class DateCalculator {
-    public static void calculate() {
-
-    }
-
-    public static void another() {
-
-    }
-    // Write your code here.
+// Write your code here. 
 }
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,7 +2,6 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        // write some testing code here.
-        System.out.println("lolz");
+        // write some testing code here. 
     }
 }
